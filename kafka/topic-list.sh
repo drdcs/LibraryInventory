917a70314a3d2202b7d9106777db9190b1efb43b
drdcs <diptihdl@gmail.com>
2020-12-23 16:53:22 +0530

    added new code for eventproducer

---
+++ kafka/topic-list.sh
@@ -0,0 +1,1 @@
+$KAFKA_HOME/bin/kafka-topics --zookeeper  localhost:2181 --list
--- src/main/java/com/datawhiz/controller/LibraryEventsController.java
+++ src/main/java/com/datawhiz/controller/LibraryEventsController.java
@@ -2,6 +2,9 @@ package com.datawhiz.controller;
 
 
 import com.datawhiz.domain.LibraryEvent;
+import com.datawhiz.producer.LibraryEventProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LibraryEventsController {
 
+    @Autowired
+    LibraryEventProducer libraryEventProducer;
+
     @PostMapping("/v1/libraryevent")
 
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent ){
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent ) throws JsonProcessingException {
 
         // invoke kafka producer
 
+        /* Serializer -> Key ser. and value Ser.
+         * Partitioner -> DefaultPartitioner.
+         * Record Accumulator.
+         * Buffer memory (linger.ms)
+         *
+         * */
+
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
 
     }
