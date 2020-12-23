ef9b2872996846476cb8c321b7d7d01591128f73
drdcs <diptihdl@gmail.com>
2020-12-23 21:25:39 +0530

    added put endpoint

--- src/main/java/com/datawhiz/controller/LibraryEventsController.java
+++ src/main/java/com/datawhiz/controller/LibraryEventsController.java
@@ -11,8 +11,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
 
 import java.util.concurrent.ExecutionException;
 
@@ -36,6 +38,18 @@ public class LibraryEventsController {
         //log.info("SendResult is {} ", sendResult.toString());
         // log.info("After sending the message..");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+
+    }
+
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> putLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent ) throws JsonProcessingException {
+
+        if(libraryEvent.getLibraryEventId()==null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("please pass Library Event ID");
+        }
+        libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
+        libraryEventProducer.sendLibraryEventAsyncWithSend(libraryEvent);
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
 
     }
 
