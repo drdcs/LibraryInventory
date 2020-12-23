b702fec6a632e659fb40f317555c8af6836b6fd4
drdcs <diptihdl@gmail.com>
2020-12-23 15:06:12 +0530

    project initialization

---
+++ src/test/java/com/datawhiz/LibraryinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datawhiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
